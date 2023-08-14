9ef675cdb14edcf424f63ba635d748e46b47d6a9
Ovi sanjaya <ovi.sanjaya11@gmail.com>
2023-08-15 02:39:36 +0700

    Update mongo.js

--- model/db/mongo.js
+++ model/db/mongo.js
@@ -4,7 +4,8 @@ export class mongoodb{
     constructor(username, password, dbName){
         // mongodb+srv://ovisannn:Vqam8WrJcYl5KCCx@cluster0.hl4u9ei.mongodb.net/?retryWrites=true&w=majority
         // this.uri = `mongodb://${uri}/${name}`
-        this.uri = `mongodb+srv://${username}:${password}@cluster0.hl4u9ei.mongodb.net/${dbName}?`
+        // mongodb://cyruslk:<MY_PASS_HERE>@franklinford-shard-00-00-3dveb.mongodb.net:27017,franklinford-shard-00-01-3dveb.mongodb.net:27017,franklinford-shard-00-02-3dveb.mongodb.net:27017/test?ssl=true&replicaSet=FranklinFord-shard-0&authSource=admin&retryWrites=true&w=majority"
+        this.uri = `mongodb+srv://${username}:${password}@cluster0.hl4u9ei.mongodb.net/${dbName}?retryWrites=true&w=majority`
     }
 
     init(){
