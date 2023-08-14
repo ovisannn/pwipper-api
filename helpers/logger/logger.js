0b84049851b96a37cd839651b68c53303e4dd6d8
Ovi sanjaya <ovi.sanjaya11@gmail.com>
2023-08-15 01:51:27 +0700

    push

--- helpers/logger/logger.js
+++ helpers/logger/logger.js
@@ -4,14 +4,5 @@ export const logger = createLogger({
     format: format.combine(
         format.timestamp({format: 'YYYY-MM-DD HH:mm:ss:ms'}),
         format.printf(info=>`${info.timestamp}`)
-    ),
-    transports: [
-        new transports.File({
-            filename: './logs/all-logs.log',
-            json: false,
-            maxFiles: 5242880,
-            maxFiles: 5
-        }),
-        new transports.Console()
-    ]
+    )
 })
